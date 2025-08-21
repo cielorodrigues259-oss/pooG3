b0dd2863efde0a197b99ad35abdd0a292b41674c
IISAA <armutoisaidd@gmail.com>
2025-08-21 12:00:11 -0500

    dalyy

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/asistenciaController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/asistenciaController.java
@@ -1,7 +1,26 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.scene.control.Label;
 import org.springframework.stereotype.Controller;
+
+ import javafx.fxml.FXML;
+ import javafx.scene.control.TextField;
+ import org.springframework.stereotype.Controller;
+
+
 
 @Controller
 public class asistenciaController {
+ @FXML TextField txtNum1,txtNum2;
+ @FXML Label txtResult;
+
+  @FXML
+ public void sumar() {
+   double Num1 = Double.parseDouble(txtNum1.getText());
+   double Num2 = Double.parseDouble(txtNum2.getText());
+   double resultado = Num1 + Num2;
+   txtResult.setText(String.valueOf(resultado));
+
+
+  }
 }
